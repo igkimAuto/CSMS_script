e001f8c7030e5b976bf0e4846f9fb3ab9cc131b7
igkim <igkim@autocrypt.io>
2022-10-07 15:04:24 +0900

    깃 경로 수정

--- frontend_script.sh
+++ frontend_script.sh
@@ -8,7 +8,7 @@ sudo docker rmi front-image
 #rm -rf CSMS_test/
 rm -rf CSMS_html/
 
-git clone git@github.com:donghyeonAuto/CSMS_html.git
+git clone git@github.com:igkimAuto/CSMS_html.git
 # git clone https://github.com/igkimAuto/CSMS_html.git
 
 sudo docker build --tag front-image ./CSMS_html/
