6172733bcda4581e62c45911155d1bbd39b8118b
igkim <igkim@autocrypt.io>
2022-10-07 14:55:10 +0900

    수정

--- CSMS_module_3.sh
+++ CSMS_module_3.sh
@@ -3,11 +3,11 @@ rm -rf CSMS_module_3
 # git clone https://github.com/igkimAuto/CSMS_module_3.git
 git clone git@github.com:igkimAuto/CSMS_module_3.git
 
-docker stop CSMS_module_3-container
-docker rm CSMS_module_3-container
-docker rmi CSMS_module_3-image
-docker build --tag CSMS_module_3-image ./CSMS_module_3
-docker run -d --name CSMS_module_3-container -p 82:82 --network fastapi-net CSMS_module_3-image
+docker stop csms_module_3-container
+docker rm csms_module_3-container
+docker rmi csms_module_3-image
+docker build --tag csms_module_3-image ./csms_module_3
+docker run -d --name csms_module_3-container -p 82:82 --network fastapi-net csms_module_3-image
 
 
 #docker exec -it vue-container bash
