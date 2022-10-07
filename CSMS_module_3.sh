9787fe1ae28f81553746b152b0e684a851ce1b52
igkim <igkim@autocrypt.io>
2022-10-07 14:38:26 +0900

    CSMS module 3 추가

---
+++ CSMS_module_3.sh
@@ -0,0 +1,11 @@
+rm -rf CSMS_module_3
+#git clone git@github.com:igkimAuto/CSMS_frontend.git
+git clone https://github.com/igkimAuto/CSMS_module_3.git
+docker stop CSMS_module_3-container
+docker rm CSMS_module_3-container
+docker rmi CSMS_module_3-image
+docker build --tag CSMS_module_3-image ./CSMS_module_3
+docker run -d --name CSMS_module_3-container -p 82:82 --network fastapi-net CSMS_module_3-image
+
+
+#docker exec -it vue-container bash
