448914d42924967fb8b9c25d0e1f626edad4d30f
igkim <igkim@autocrypt.io>
2022-10-07 14:56:42 +0900

    경로 수정

--- CSMS_module_3.sh
+++ CSMS_module_3.sh
@@ -6,7 +6,7 @@ git clone git@github.com:igkimAuto/CSMS_module_3.git
 docker stop csms_module_3-container
 docker rm csms_module_3-container
 docker rmi csms_module_3-image
-docker build --tag csms_module_3-image ./csms_module_3
+docker build --tag csms_module_3-image ./CSMS_module_3
 docker run -d --name csms_module_3-container -p 82:82 --network fastapi-net csms_module_3-image
 
 
